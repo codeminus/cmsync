06fa573d1fa81a0732b145e567252cc4db9713c4
Wilson Santos <wss@slb.com>
2016-08-29 16:32:03 -0500

    first commit

---
+++ cmsync/sync.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3
+
+import json
+import os
+
+from color import Color
+from transport import Transport
+
+
+class Sync:
+    """Sync directories and files based on the specifications of the json file
+    """
+
+    def __init__(self, config_file):
+        self._load_config(config_file)
+
+    def _load_config(self, config_file):
+        """Loads the configuration file"""
+
+        if not os.path.isfile(config_file):
+            print(Color.ERROR + '{} not found.'.format(config_file))
+            quit()
+
+        self._config = json.load(open(config_file))
+
+    def start(self):
+        """Starts synchronization"""
+        for entry in self._config['manifest']:
+
+            t = Transport(
+                entry['src'],
+                entry['dest'],
+                sftp_config
+            )
+            t.send()
+
+            if 'run_after' in entry.keys():
+                print(Color.COMMAND + "Running `{}`".format(entry['run_after']))
+                os.system(entry['run_after'])
+
+if __name__ == '__main__':
+    s = Sync('sync.json')
+    s.start()
