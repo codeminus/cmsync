6a7f2615eca0af3c67d60665d39d1707530a20fa
Wilson Santos <wilson@codeminus.org>
2016-09-01 20:59:28 -0500

    Package documentation improved. Minor code refactoring

--- cmsync/transport.py
+++ cmsync/transport.py
@@ -7,17 +7,25 @@ from .color import Color
 
 
 class Transport:
-    """
-    """
+    """Copies files from one destination to another"""
 
     def __init__(self, src, dest):
+        """
+        Initializes a Transport instance.
+        Args:
+            src (str): The source path of the file or folder to the copied.
+            dest (str): The destination file or folder to copy the source
+                contents over.
+        """
         self.src = src
         self.dest = dest
 
         self.valid = None
 
     def is_valid(self):
-        """
+        """Checks if the manifest of the transport is valid.
+
+        Implementation is going to be more important when SFTP support is added.
         """
         if not os.path.exists(self.src):
             print(
@@ -27,8 +35,18 @@ class Transport:
 
         return True
 
-    def _remove_readonly(self, func, path, _):
-        """
+    @staticmethod
+    def _remove_readonly(func, path, _):
+        """Removes the readonly property of a file or folder.
+
+        This method was created focused on handling errors when removing
+        folders with shutil.rmtree.
+
+        Args:
+            func: the function to be called after the readonly flag is removed.
+            path: file or directory path.
+            _: captures the remaining, unnecessary parameters.
+
         """
         print('')
         print("Clearing readonly bit and reattempt removal of '{}'".format(path))
@@ -36,16 +54,19 @@ class Transport:
         func(path)
 
     def send(self):
-        """
+        """Validates the manifest and sends the transport to destination.
+
+        When SFTP is implemented, it will identify if it should use `send_local`
+        or `send_remote` for transport.
         """
         if self.valid is None:
             if not self.is_valid():
                 return False
 
         self.send_local()
-            
 
     def send_local(self):
+        """Copies the source to a local destination."""
 
         # If source and destination are files
         if os.path.isfile(self.src) and os.path.isfile(self.dest):
@@ -73,7 +94,7 @@ class Transport:
                 "Deleting folder '{}'... ".format(self.dest),
                 end=''
             )
-            shutil.rmtree(self.dest, onerror=self._remove_readonly)
+            shutil.rmtree(self.dest, onerror=Transport._remove_readonly)
             print('Done.')
 
         print('')
